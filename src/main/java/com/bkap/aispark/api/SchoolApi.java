15de4a4c7a84d6dd1702baa978d67dcf66b5eda2
NguyenQChung <chungnguyenquang2001@gmail.com>
2025-09-01 03:45:26 +0700

    Them , Sua , Xoa truong qua id

--- src/main/java/com/bkap/aispark/api/SchoolApi.java
+++ src/main/java/com/bkap/aispark/api/SchoolApi.java
@@ -3,8 +3,12 @@ package com.bkap.aispark.api;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +36,36 @@ public class SchoolApi {
     public ResponseEntity<Schools> getSchoolById(@PathVariable Long id) {
         return schoolRepository.findById(id)
                 .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Them 1 truong
+    @PostMapping
+    public Schools createSchool(@RequestBody Schools school) {
+        return schoolRepository.save(school);
+    }
+
+
+    // Sua thong tin truong qua ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Schools> updateSchool(@PathVariable Long id, @RequestBody Schools schoolDetails) {
+        return schoolRepository.findById(id)
+                .map(school -> {
+                    school.setName(schoolDetails.getName());
+                    school.setAddress(schoolDetails.getAddress());
+                    return ResponseEntity.ok(schoolRepository.save(school));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Xoa 1 trường qua id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSchool(@PathVariable Long id) {
+        return schoolRepository.findById(id)
+                .map(school -> {
+                    schoolRepository.delete(school);
+                    return ResponseEntity.noContent().<Void>build();
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 
