198fe7acfffbba7589bb53633e1dc0ab32f8eda5
Pham Nhu Son <phamnhusondolla@gmail.com>
2025-12-03 15:03:57 +0700

    Tách css js tu các trang html

--- src/main/java/com/bkap/aispark/config/CorsConfig.java
+++ src/main/java/com/bkap/aispark/config/CorsConfig.java
@@ -3,6 +3,9 @@ package com.bkap.aispark.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -15,5 +18,10 @@ public class CorsConfig implements WebMvcConfigurer {
 				.allowedMethods("*")
 				.allowedHeaders("*")
 				.allowCredentials(true);
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/.well-known/**")
+				.addResourceLocations("classpath:/static/.well-known/");
 	}
 }
